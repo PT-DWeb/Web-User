71988ad30ac319823c7f36135e6b512cca57d469
lhdong123 <lyhandong123@gmail.com>
2020-12-04 14:21:09 +0700

    chua sua mot so anh

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var electronicController = require('../controllers/eletronicController');
+//const usersController = require('../controllers/usersController');
+
+/* GET home page. */
+console.log("index.js");
+router.get('/', electronicController.product);
+router.get('/about', electronicController.about);
+router.get('/contact', electronicController.contact);
+
+module.exports = router;
